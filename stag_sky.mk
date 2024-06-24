3720bb0adc9b1037a056816cd99f15aa1856c0e3
solo-captain-leveling <106550362+dhanush281@users.noreply.github.com>
2024-06-23 18:16:46 -0700

    sky: stagos bringup!

--- stag_sky.mk
+++ stag_sky.mk
@@ -13,7 +13,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit common stagos configurations
-$(call inherit-product, vendor/stag/config/common_full_phone.mk)
+$(call inherit-product, vendor/stag/config/main.mk)
 
 # Inherit device configurations
 $(call inherit-product, $(DEVICE_PATH)/device.mk)
