627dece256890d8867452c2027be2a65e23708a1
dhanush281 <dhanushvozh345@gmail.com>
2024-12-11 07:46:00 +0530

    welcome to 2by2

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,8 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_sky.mk
+    $(LOCAL_DIR)/lineage_sky.mk
+COMMON_LUNCH_CHOICES := \
+    lineage_sky-ap3a-user \
+    lineage_sky-ap3a-userdebug \
+    lineage_sky-ap3a-eng
--- lineage_sky.mk
+++ lineage_sky.mk
@@ -12,8 +12,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 # Configure full_base_telephony.mk
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit common PixelOS configurations
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit common 2by2project configurations
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit device configurations
 $(call inherit-product, $(DEVICE_PATH)/device.mk)
@@ -21,9 +21,21 @@ $(call inherit-product, $(DEVICE_PATH)/device.mk)
 # Inherit from the proprietary version
 $(call inherit-product, vendor/xiaomi/sky/sky-vendor.mk)
 
+# 2by2 flags
+WITH_GMS := true
+CUSTOM_BUILD_TYPE := OFFICIAL
+CUSTOM_MAINTAINER := Dhanush
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_INCLUDE_EXTRA_APPS := false
+TARGET_EXCLUDES_AUDIOFX := true
+FORCE_AOSP_CONTACTS := false
+TARGET_SUPPORTS_64_BIT_APPS := true
+TARGET_SCREEN_WIDTH := 1080
+TARGET_SCREEN_HEIGHT := 2460
+
 ## Device identifier
 PRODUCT_DEVICE := sky
-PRODUCT_NAME := aosp_sky
+PRODUCT_NAME := lineage_sky
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := 23076RN4BI
 PRODUCT_MANUFACTURER := Xiaomi
